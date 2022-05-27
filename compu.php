4db093c6d85a1a5c60a21f094a15e4f30000d139
GodSavitar19 <106354483+GodSavitar19@users.noreply.github.com>
2022-05-26 21:03:16 -0300

    Update compu.php

--- compu.php
+++ compu.php
@@ -1,7 +1,11 @@
 Modificacion
 Una Computadora tiene infinidad de utilidades
+<<<<<<< Updated upstream
 Linea que espero que no haga conflicto
 
 
 asdasdasdasd
+=======
+Linea que espero que no haga conflicto, si hizo conflicto
+>>>>>>> Stashed changes
 
