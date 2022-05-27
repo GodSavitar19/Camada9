5afb280203d6470837cb9b61654f2546aefcbb4c
TechSergio <smarquez15@gmail.com>
2022-05-27 18:26:15 -0300

    se unieron ambos cambios

--- compu.php
+++ compu.php
@@ -1,11 +1,8 @@
 Modificacion
 Una Computadora tiene infinidad de utilidades
-<<<<<<< Updated upstream
 Linea que espero que no haga conflicto
 
 
 asdasdasdasd
-=======
 Linea que espero que no haga conflicto, si hizo conflicto
->>>>>>> Stashed changes
 
